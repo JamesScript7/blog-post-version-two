b17b6f53f380d1fa77de67c1b1eb2086fce6f2d4
James Kim <james.kim7@yahoo.com>
2017-01-14 20:16:41 -0500

    no real changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,23 @@
 $(function() {
 
 	$("#submitForm").submit(function(event) {
+		event.preventDefault();
 
 		var formData = {
 			'title': $('input[name=title]').val(),
-			'comment': $('input[name=comment]').val()
+			'comment': $('input[name=comment]').val(),
+			'date': new Date()
 		}
-
-		// console.log(formData.title);
-		// console.log(formData.comment);
-
 		
 		$.post('/blog', formData, function(res) {
 			console.log(res);
 
-			$("#blogPosts").text(res.title);
-			$("#blogPosts").text(res.comment);
+			$("#blogTitle").text(res.title);
+			$("#blogComment").text(res.comment);
+			$("#blogDate").text(res.date);
+
 		})
 
-		event.preventDefault();
 	});
 
 
